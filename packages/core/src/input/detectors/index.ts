83ea710f849c693da3a9b96da1b5d807098a9c98
devlopersumit <sumitsj2002@gmail.com>
2026-09-06 17:40:18 +0530

    Export input detector modules

---
+++ packages/core/src/input/detectors/index.ts
@@ -0,0 +1,4 @@
+export { detectSqlInjection, getSqlPatterns, SQL_BASE_PATTERNS, SQL_DIALECT_PATTERNS } from './sql.js';
+export { detectXss, getXssPatterns, XSS_HTML_PATTERNS, XSS_CONTEXT_PATTERNS } from './xss.js';
+export { detectSecrets, SECRET_PATTERNS } from './secrets.js';
+export { detectPathTraversal } from './path-traversal.js';
