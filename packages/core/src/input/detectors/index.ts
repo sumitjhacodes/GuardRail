65cd1607ec6f775ffdade06985c031ff5f8b663d
devlopersumit <sumitsj2002@gmail.com>
2026-09-09 18:14:17 +0530

    Export pattern accelerator APIs from detectors

--- packages/core/src/input/detectors/index.ts
+++ packages/core/src/input/detectors/index.ts
@@ -2,3 +2,10 @@ export { detectSqlInjection, getSqlPatterns, SQL_BASE_PATTERNS, SQL_DIALECT_PATT
 export { detectXss, getXssPatterns, XSS_HTML_PATTERNS, XSS_CONTEXT_PATTERNS } from './xss.js';
 export { detectSecrets, SECRET_PATTERNS } from './secrets.js';
 export { detectPathTraversal } from './path-traversal.js';
+export {
+  setPatternAccelerator,
+  getPatternAccelerator,
+  matchSqlWithAccelerator,
+  type PatternAccelerator,
+  type PatternMatch,
+} from './accelerator.js';
